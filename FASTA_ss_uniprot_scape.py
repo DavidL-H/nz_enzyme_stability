1183b7ac8d35bc764eb689e6ffe94b307c5fad11
David Lennox-Hvenekilde <davidlh3@gmail.com>
2022-10-01 14:54:29 +0200

    ready to run on linux

--- FASTA_ss_uniprot_scape.py
+++ FASTA_ss_uniprot_scape.py
@@ -16,36 +16,36 @@ train_df = pd.read_csv("./data/train_updated.csv")
 train_df.head()
 
 # Add new columns
-train_df['uniprot_id'] = pd.NA
-train_df['protein_id'] = pd.NA
-train_df['protein_name'] = pd.NA
-train_df['length'] = pd.NA
-train_df['not_sure'] = pd.NA
-train_df['score_bits'] = pd.NA
-train_df['e_val'] = pd.NA
+train_df['fasta_ss_search_result'] = pd.NA
 
 # Test
-test_seq = train_df["protein_sequence"][0]
-jobID = fas.post_fasta_ss_search(fas.aaseq_to_request_string(test_seq, "test_title"))
-# Wait til job is done, before getting results
-while not fas.status_fasta_ss_search(jobID):
-    print(jobID + " still running. Waiting 2 seconds")
-    time.sleep(2)
-print("Job done, getting results")
-job_result_string = fas.get_plain_text_result(jobID)
-job_result = job_result_string.split(" ")
+for n in range(len(train_df)):
+    test_seq = train_df["protein_sequence"][n]
+    jobID = fas.post_fasta_ss_search(fas.aaseq_to_request_string(test_seq, "test_title"))
+    # Wait til job is done, before getting results
+    while not fas.status_fasta_ss_search(jobID):
+        print(jobID + " still running. Waiting 2 seconds")
+        time.sleep(2)
+    print("Job done, getting results")
+    train_df['fasta_ss_search_result'][n] = fas.get_plain_text_result(jobID)
+    # Save results
+    train_df.to_csv("./data/train_updated_uniprot_ss_search.csv")
 
+
+
+
+# For later
 # Extract key data from ss search
-uniprot_id = job_result[0]
-protein_id = job_result[1]
+'''
+train_df["uniprot_id"][n] = job_result[0]
+train_df["protein_id"][n]  = job_result[1]
 if len(job_result) > 7:
-    protein_name = " ".join(job_result[2:(len(job_result)-5)])
+    train_df["protein_name"][n] = " ".join(job_result[2:(len(job_result)-5)])
 else:
-    protein_name = job_result[2]
-e_val = float(job_result[-1])
-score_bits = float(job_result[-2])
-not_sure = int(job_result[-3])
-length = int(job_result[-4][:-1])
+    train_df["protein_name"][n] = job_result[2]
+train_df["e_val"][n] = float(job_result[-1])
+train_df["score_bits"][n] = float(job_result[-2])
+train_df["not_sure"][n] = int(job_result[-3])
+train_df['length'][0] = int(job_result[-4][:-1])
+'''
 
-
-train_df.to_csv("./data/train_updated_unirpto_ss_search.csv")
