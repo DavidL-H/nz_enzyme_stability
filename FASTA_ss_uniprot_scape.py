63af9e510ab70d98a9f648902586513c5db17160
David Lennox-Hvenekilde <davidlh3@gmail.com>
2022-10-15 13:59:18 +0200

    Final ss search data

--- FASTA_ss_uniprot_scape.py
+++ FASTA_ss_uniprot_scape.py
@@ -34,19 +34,25 @@ for n in range(28944, len(train_df)):
 
 
 
-
-# For later
-# Extract key data from ss search
 '''
-train_df["uniprot_id"][n] = job_result[0]
-train_df["protein_id"][n]  = job_result[1]
-if len(job_result) > 7:
-    train_df["protein_name"][n] = " ".join(job_result[2:(len(job_result)-5)])
-else:
-    train_df["protein_name"][n] = job_result[2]
-train_df["e_val"][n] = float(job_result[-1])
-train_df["score_bits"][n] = float(job_result[-2])
-train_df["not_sure"][n] = int(job_result[-3])
-train_df['length'][0] = int(job_result[-4][:-1])
+Second run of the API Fasta BLAST search using the missing values data frame,
+The output of "./data/data_clean.py"
 '''
 
+# Rerun the search with the missing values dataframe
+train_df_missing = pd.read_csv("./data/train_updated_uniprot_ss_search_missing.csv")
+train_df_missing.head()
+train_df_missing['fasta_ss_search_result'] = pd.NA
+
+for n in range(len(train_df_missing)):
+    test_seq = train_df_missing["protein_sequence"][n]
+    jobID = fas.post_fasta_ss_search(fas.aaseq_to_request_string(test_seq, "test_title"))
+    # Wait til job is done, before getting results
+    while not fas.status_fasta_ss_search(jobID):
+        print(jobID + " still running. Waiting 2 seconds")
+        time.sleep(2)
+    print("Job done, getting results")
+    train_df_missing['fasta_ss_search_result'][n] = fas.get_plain_text_result(jobID)
+    # Save results
+    print("Sequence nr. " + str(n) + " saved...")
+    train_df_missing.to_csv("./data/train_updated_uniprot_ss_search_v10.csv")
